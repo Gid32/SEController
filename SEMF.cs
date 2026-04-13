f816c8985baff4ce05586f2fce8fe333098749e4
GiD32 <ivn_prmsk@ukr.net>
2026-04-13 13:39:06 +0300

    Refactor SEMF.cs for improved code clarity and maintainability
    changed refinery item transfer to move one item at a time, multiple item transfer was causing instruction limit issues
    (refynery has continuous output, so it was trying to move items as fast as they were produced, which could lead to a large number of items being moved in a single tick).
    added comments for better understanding, and removed redundant code.

--- SEMF.cs
+++ SEMF.cs
@@ -1,50 +1,55 @@
-int 	cR, cG, cB; 
-int		LightRadius;
+// SEMF - Space Engineers Management Framework
+// https://malforge.github.io/spaceengineers/pbapi/
 
-string 	StorageMaterialsKeyword; 
-string 	StorageComponentsKeyword; 
+int cR, cG, cB;
+int LightRadius;
 
-string 	SolarPointerRCKeyword;
-string 	SolarRotorKeyword;
-string 	SolarPanelKeyword;
-int    	SolarRedirects;
-double 	Proximity, SolarDirectionSwitch;
-double 	SolarPowerOutput, SolarPowerOutputPrev;
+string StorageMaterialsKeyword;
+string StorageComponentsKeyword;
+
+string SolarPointerRCKeyword;
+string SolarRotorKeyword;
+string SolarPanelKeyword;
+int SolarRedirects;
+double Proximity, SolarDirectionSwitch;
+double SolarPowerOutput, SolarPowerOutputPrev;
 
 ////////////////////////////////////// LISTS ///////////////////////////////////
-List<IMyInteriorLight> LightBlocks = new List<IMyInteriorLight>(); 
+List<IMyInteriorLight> LightBlocks = new List<IMyInteriorLight>();
 
 List<IMyAssembler> assemblers = new List<IMyAssembler>();
-List<IMyRefinery> refinerys = new List<IMyRefinery>();   
+List<IMyRefinery> refinerys = new List<IMyRefinery>();
 
 List<MyInventoryItem> items = new List<MyInventoryItem>();
-List<IMyTerminalBlock> L = new List<IMyTerminalBlock>(); 
+List<IMyTerminalBlock> L = new List<IMyTerminalBlock>();
 ////////////////////////////////////// LISTS ///////////////////////////////////
 
-int     Ticker;
-public Program(){
-    Runtime.UpdateFrequency = UpdateFrequency.Update10; //| UpdateFrequency.Update100 | UpdateFrequency.Update1
+int Ticker;
+public Program()
+{
+	Runtime.UpdateFrequency = UpdateFrequency.Update10; //| UpdateFrequency.Update100 | UpdateFrequency.Update1
 	Ticker = 0;
-// Light -----------------------------------------------------------------------
+	// Light -----------------------------------------------------------------------
 	cR = 204;
 	cG = 255;
 	cB = 140;
 	LightRadius = 10;
-// Storage -----------------------------------------------------------------------
-	StorageMaterialsKeyword 	= "[Materials]"; 
-	StorageComponentsKeyword 	= "[Components]"; 
-// Solar -----------------------------------------------------------------------
+	// Storage -----------------------------------------------------------------------
+	StorageMaterialsKeyword = "[Materials]";
+	StorageComponentsKeyword = "[Components]";
+	// Solar -----------------------------------------------------------------------
 	SolarPointerRCKeyword = "[Axis Aligner]";
-	SolarRotorKeyword     = "[Solar Wing]";
-	SolarPanelKeyword     = "Solar Panel";
+	SolarRotorKeyword = "[Solar Wing]";
+	SolarPanelKeyword = "Solar Panel";
 	SolarDirectionSwitch = 1;
-	SolarRedirects =0;
+	SolarRedirects = 0;
 	Proximity = 0.001;
 }
 
 //public void Save(){}
 
-public void Main(string argument, UpdateType updateSource){    
+public void Main(string argument, UpdateType updateSource)
+{
 	if ((updateSource & (UpdateType.Update10)) != 0 && Ticker < 6)
 	{
 		Ticker++;
@@ -54,124 +59,132 @@ public void Main(string argument, UpdateType updateSource){
 	{
 		Ticker = 0;
 	}
-	
-	LightAdjust(cR, cG, cB); 
-	CleanProductionInventories(); 
+
+	LightAdjust(cR, cG, cB);
+	CleanProductionInventories();
 	SolarAdjust();
-	
-}  
+
+}
 
 // ------------------------------------------------------------------------------- Light
 
-void LightAdjust(int R, int G, int B){ 
-    GridTerminalSystem.GetBlocksOfType<IMyInteriorLight>(LightBlocks); 
-	 
-	var norm = new Color(R,G,B,255); 
-	for (int i = 0; i < LightBlocks.Count; i++) 
-	{ 
+void LightAdjust(int R, int G, int B)
+{
+	GridTerminalSystem.GetBlocksOfType<IMyInteriorLight>(LightBlocks);
+
+	var norm = new Color(R, G, B, 255);
+	for (int i = 0; i < LightBlocks.Count; i++)
+	{
 		LightBlocks[i].Radius = LightRadius;
-		LightBlocks[i].Color = norm; 
-	} 
-} 
+		LightBlocks[i].Color = norm;
+	}
+}
 
 // ------------------------------------------------------------------------------- Storage
 
-void CleanProductionInventories(){  
-      
-    var invMatBock = BlockNamed(StorageMaterialsKeyword);  
-    IMyInventory MaterialsInventory = (invMatBock != null) ? invMatBock.GetInventory(0) : null;   
-      
-    var invCompBock = BlockNamed(StorageComponentsKeyword);  
-    IMyInventory ComponentsInventory = (invCompBock != null) ? invCompBock.GetInventory(0) : null;  
-    
-	CleanAssemblers (ComponentsInventory, MaterialsInventory);
-	CleanRefinerys (MaterialsInventory);
-}  
+void CleanProductionInventories()
+{
 
-void CleanAssemblers (IMyInventory ComponentsInventory, IMyInventory MaterialsInventory){
-    GridTerminalSystem.GetBlocksOfType<IMyAssembler>(assemblers);  
-    
-	for (int i = 0; i < assemblers.Count; i++)  
-	{  
-		CleanAssembler(assemblers[i] as IMyAssembler, ComponentsInventory, MaterialsInventory); 
-    }  
+	var invMatBock = BlockNamed(StorageMaterialsKeyword);
+	IMyInventory MaterialsInventory = (invMatBock != null) ? invMatBock.GetInventory(0) : null;
+
+	var invCompBock = BlockNamed(StorageComponentsKeyword);
+	IMyInventory ComponentsInventory = (invCompBock != null) ? invCompBock.GetInventory(0) : null;
+
+	CleanAssemblers(ComponentsInventory, MaterialsInventory);
+	CleanRefinerys(MaterialsInventory);
 }
-  
-void CleanAssembler(IMyAssembler assembler, IMyInventory ComponentsInventory, IMyInventory MaterialsInventory){  
-    if (assembler != null)  
-    {  
+
+void CleanAssemblers(IMyInventory ComponentsInventory, IMyInventory MaterialsInventory)
+{
+	GridTerminalSystem.GetBlocksOfType<IMyAssembler>(assemblers);
+
+	for (int i = 0; i < assemblers.Count; i++)
+	{
+		CleanAssembler(assemblers[i] as IMyAssembler, ComponentsInventory, MaterialsInventory);
+	}
+}
+
+void CleanAssembler(IMyAssembler assembler, IMyInventory ComponentsInventory, IMyInventory MaterialsInventory)
+{
+	if (assembler != null)
+	{
 		IMyInventory source, result, sourceStorage, resultStorage;
 		if (assembler.Mode == MyAssemblerMode.Assembly)
 		{
-			source = assembler.GetInventory(0); 
+			source = assembler.GetInventory(0);
 			sourceStorage = MaterialsInventory;
-			
+
 			result = assembler.GetInventory(1);
 			resultStorage = ComponentsInventory;
 		}
 		else
 		{
 			source = assembler.GetInventory(1);
-			sourceStorage = ComponentsInventory; 
-			
+			sourceStorage = ComponentsInventory;
+
 			result = assembler.GetInventory(0);
 			resultStorage = MaterialsInventory;
 		}
-		 
-		
-        if (resultStorage != null)  
+
+
+		if (resultStorage != null)
 		{
 			MoveAllItems(result, resultStorage);
 		}
-		
-		if (!assembler.IsProducing && sourceStorage != null)  
+
+		if (!assembler.IsProducing && sourceStorage != null)
 		{
-			MoveAllItems(source, sourceStorage); 
+			MoveAllItems(source, sourceStorage);
 		}
-    }
-}  
-  
-void CleanRefinerys(IMyInventory MaterialsInventory){ 
-    GridTerminalSystem.GetBlocksOfType<IMyRefinery>(refinerys);  
-    if (refinerys != null && MaterialsInventory != null)  
-    {  
-        for (int i = 0; i < refinerys.Count; i++)  
-        {  
-            CleanRefinery(refinerys[i] as IMyRefinery, MaterialsInventory);  
-        }  
-    }  
+	}
 }
 
-void CleanRefinery(IMyRefinery refinery, IMyInventory MaterialsInventory){  
-    if (refinery != null)  
-    {   
-		MoveAllItems(refinery.GetInventory(1), MaterialsInventory);
-    }
-}  
+void CleanRefinerys(IMyInventory MaterialsInventory)
+{
+	GridTerminalSystem.GetBlocksOfType<IMyRefinery>(refinerys);
+	if (refinerys != null && MaterialsInventory != null)
+	{
+		for (int i = 0; i < refinerys.Count; i++)
+		{
+			CleanRefinery(refinerys[i] as IMyRefinery, MaterialsInventory);
+		}
+	}
+}
+
+void CleanRefinery(IMyRefinery refinery, IMyInventory MaterialsInventory)
+{
+	if (refinery != null)
+	{
+		// MoveAllItems(refinery.GetInventory(1), MaterialsInventory);
+		MoveOneItem(refinery.GetInventory(1), MaterialsInventory);
+	}
+}
 
 // ------------------------------------------------------------------------------- Solar
 
-void SolarAdjust(){
-	IMySolarPanel 	 solar 		= BlockNamed(SolarPanelKeyword) 	as IMySolarPanel;
-	IMyMotorStator 	 rotor 		= BlockNamed(SolarRotorKeyword) 	as IMyMotorStator;
-	
+void SolarAdjust()
+{
+	IMySolarPanel solar = BlockNamed(SolarPanelKeyword) as IMySolarPanel;
+	IMyMotorStator rotor = BlockNamed(SolarRotorKeyword) as IMyMotorStator;
+
 	SolarConfigureAxisAligner();
-	
+
 	if (solar != null)
 	{
 		SolarPowerOutputPrev = SolarPowerOutput;
 		SolarPowerOutput = solar.MaxOutput / 0.16;
 		double delta = SolarPowerOutput - SolarPowerOutputPrev;
-		
-		
+
+
 		Echo("Solar Panels");
 		Echo("Power: " + (SolarPowerOutput * 100) + "%");
 		Echo("Delta: " + (delta * 100) + "%");
-		
+
 		Echo("Power Gen.: " + (solar.MaxOutput * 1000) + " kW");
 		Echo("Power Use.: " + (solar.CurrentOutput * 1000) + " kW");
 
-    if (delta < 0 && Math.Abs(delta) > 3.0 * Proximity)
+		if (delta < 0 && Math.Abs(delta) > 3.0 * Proximity)
 		{
 			SolarRedirects++;
 			SolarDirectionSwitch = SolarDirectionSwitch * (-1.0 / SolarRedirects);
@@ -186,68 +199,82 @@ void SolarAdjust(){
 		if (rotor != null)
 		{
 			rotor.BrakingTorque = rotor.Torque;
-			
+
 			if ((1.0 - Proximity) > SolarPowerOutput && SolarPowerOutput != 0)
 			{
 				rotor.TargetVelocityRPM = (float)(2 * SolarDirectionSwitch * (1 - SolarPowerOutput));
 			}
-			else 
+			else
 			{
 				rotor.TargetVelocityRPM = 0;
 				SolarDirectionSwitch = Math.Sign(SolarDirectionSwitch);
 			}
-			
+
 			Echo("Angle: " + rotor.Angle + " rad.");
 			Echo("Velocity: " + rotor.TargetVelocityRPM + " RPM");
 		}
 	}
 }
 
-void SolarConfigureAxisAligner(){
+void SolarConfigureAxisAligner()
+{
 	double[] xyz;
 	IMyRemoteControl controller = BlockNamed(SolarPointerRCKeyword) as IMyRemoteControl;
-	
+
 	if (controller != null)
 	{
 		xyz = GetXYZ(controller);
 		controller.ClearWaypoints();
 		controller.ControlThrusters = false;
 		controller.FlightMode = FlightMode.OneWay;
-		controller.AddWaypoint(new Vector3D(xyz[0], xyz[1] + 1000000000, xyz[2]),"SOL");
+		controller.AddWaypoint(new Vector3D(xyz[0], xyz[1] + 1000000000, xyz[2]), "SOL");
 	}
 }
 
 // ------------------------------------------------------------------------------- util
 
-void MoveAllItems(IMyInventory source, IMyInventory dest){
+void MoveAllItems(IMyInventory source, IMyInventory dest)
+{
 	source.GetItems(items);
-	
-	for (int i = items.Count -1; i >= 0; i--)  
-	{  
-		source.TransferItemTo(dest, i, null, true, null);  
+
+	for (int i = items.Count - 1; i >= 0; i--)
+	{
+		source.TransferItemTo(dest, i, null, true, null);
 	}
 }
 
-double[] GetXYZ(IMyTerminalBlock block){   
-    return new double[]{block.GetPosition().GetDim(0), block.GetPosition().GetDim(1), block.GetPosition().GetDim(2)};   
-}   
-   
-void SetName(IMyTerminalBlock block, string name){   
-    if (block != null) {    
-        block.CustomName = name;    
-    }   
-}   
-   
-List<IMyTerminalBlock> BlocksNamed(String str){     
-    GridTerminalSystem.SearchBlocksOfName(str,L);   
-    return L;   
-}   
+void MoveOneItem(IMyInventory source, IMyInventory dest)
+{
+	source.GetItems(items);
+  source.TransferItemTo(dest, 0, null, true, null);
+}
 
-IMyTerminalBlock BlockNamed(String str){   
-    List<IMyTerminalBlock> L = BlocksNamed(str);   
-    return L.Count>0?L[0]:null;   
-}   
+double[] GetXYZ(IMyTerminalBlock block)
+{
+	return new double[] { block.GetPosition().GetDim(0), block.GetPosition().GetDim(1), block.GetPosition().GetDim(2) };
+}
 
-void assert(bool cond, String errormsg){   
-    if(!cond) throw new Exception(errormsg);   
+void SetName(IMyTerminalBlock block, string name)
+{
+	if (block != null)
+	{
+		block.CustomName = name;
+	}
+}
+
+List<IMyTerminalBlock> BlocksNamed(String str)
+{
+	GridTerminalSystem.SearchBlocksOfName(str, L);
+	return L;
+}
+
+IMyTerminalBlock BlockNamed(String str)
+{
+	List<IMyTerminalBlock> L = BlocksNamed(str);
+	return L.Count > 0 ? L[0] : null;
+}
+
+void assert(bool cond, String errormsg)
+{
+	if (!cond) throw new Exception(errormsg);
 }
