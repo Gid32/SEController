2c985854bb8875102d299d7af3b3b54c4e4d8e2f
GiD32 <ivn_prmsk@ukr.net>
2026-04-19 04:34:07 +0300

    refactor solar management code and improve variable naming for clarity
    added ctc support for solar tracking, display text shows if ctc is active
    added setting clock on every refil station
    added desc sort on resources display
    added total power from solar panels calculation
    changed direct color storage instead of components
    changed

--- SEMF.cs
+++ SEMF.cs
@@ -1,7 +1,5 @@
 // SEMF - Space Engineers Management Framework
 // https://malforge.github.io/spaceengineers/pbapi/
-
-int cR, cG, cB;
 int LightRadius;
 
 string StorageMaterialsKeyword;
@@ -10,40 +8,54 @@ string StorageMaterialsDisplayKeyword;
 string StorageComponentsDisplayKeyword;
 int StorageMaterialsDisplayPanel;
 int StorageComponentsDisplayPanel;
+
 string AlgaeFarmKeyword;
 
 string SolarPointerRCKeyword;
-string SolarRotorKeyword;
-string SolarPanelKeyword;
+string solarAzimuthRotorKeyword;
 string SolarDisplayKeyword;
+string SolarCTCKeyword;
+string SolarCameraKeyword;
 int SolarDisplaypanel;
 int SolarRedirects;
 double Proximity, SolarDirectionSwitch;
 double SolarPowerOutput, SolarPowerOutputPrev;
 
 ////////////////////////////////////// LISTS ///////////////////////////////////
+List<IMyTerminalBlock> L = new List<IMyTerminalBlock>();
+
 List<IMyReflectorLight> ReflectorLightBlocks = new List<IMyReflectorLight>();
 List<IMyInteriorLight> InteriorLightBlocks = new List<IMyInteriorLight>();
+List<IMyLightingBlock> interiorLightsCast = new List<IMyLightingBlock>();
+List<IMyLightingBlock> reflectorLightsCast = new List<IMyLightingBlock>();
 
 List<IMyAssembler> assemblers = new List<IMyAssembler>();
 List<IMyRefinery> refinerys = new List<IMyRefinery>();
 
-List<IMyLightingBlock> interiorLightsCast = new List<IMyLightingBlock>();
-List<IMyLightingBlock> reflectorLightsCast = new List<IMyLightingBlock>();
+List<IMyMedicalRoom> RefillPoints = new List<IMyMedicalRoom>();
+
 List<MyInventoryItem> items = new List<MyInventoryItem>();
-List<IMyTerminalBlock> L = new List<IMyTerminalBlock>();
+List<IMySolarPanel> solarPanels = new List<IMySolarPanel>();
+// List<IMySolarFoodGenerator> algaeFarms = new List<IMySolarFoodGenerator>();
 List<IMyTerminalBlock> algaeFarms = new List<IMyTerminalBlock>();
 ////////////////////////////////////// LISTS ///////////////////////////////////
+
+Color interiorLightColor;
+Color reflectorLightColor;
+Color foregroundColor;
+Color backgroundColor;
 
 ////////////////////////////////////// CACHED BLOCKS ///////////////////////////////////
 IMyTerminalBlock invMatBlock;
 IMyTerminalBlock invCompBlock;
-IMySolarPanel solarPanel;
-IMyMotorStator solarRotor;
+IMyMotorStator solarAzimuthRotor;
 IMyRemoteControl solarController;
+IMyCameraBlock solarCamera;
 IMyTextSurface solarDisplay;
+IMyTurretControlBlock solarCTC;
 IMyTextSurface materialsDisplay;
 IMyTextSurface componentsDisplay;
+
 System.Text.StringBuilder _sb = new System.Text.StringBuilder();
 ////////////////////////////////////// CACHED BLOCKS ///////////////////////////////////
 
@@ -58,10 +70,12 @@ public Program()
 	Runtime.UpdateFrequency = UpdateFrequency.Update10; //| UpdateFrequency.Update10 | UpdateFrequency.Update1
 	Ticker = 0;
 	// Light -----------------------------------------------------------------------
-	cR = 204;
-	cG = 255;
-	cB = 140;
+	interiorLightColor = new Color(204, 255, 140, 255);
+	reflectorLightColor = new Color(255, 255, 255, 255);
 	LightRadius = 10;
+	// Clocks ----------------------------------------------------------------------
+	foregroundColor = new Color(100, 255, 100, 255);
+	backgroundColor = new Color(0, 0, 0, 255);
 	// Storage ---------------------------------------------------------------------
 	StorageMaterialsKeyword = "[Materials]";
 	StorageMaterialsDisplayKeyword = "[MatDisplay]";
@@ -74,9 +88,11 @@ public Program()
 	AlgaeFarmKeyword = "Algae Farm";
 	// Solar -----------------------------------------------------------------------
 	SolarPointerRCKeyword = "[Axis Aligner]";
-	SolarRotorKeyword = "[Solar Wing]";
-	SolarPanelKeyword = "Solar Panel";
+	solarAzimuthRotorKeyword = "[Solar Wing]";
 	SolarDisplayKeyword = "[SolarDisplay]";
+	SolarCTCKeyword = "[SolarCTC]";
+	SolarCameraKeyword = "[SolarTracker]";
+
 	SolarDisplaypanel = 2;
 	SolarDirectionSwitch = 1;
 	SolarRedirects = 0;
@@ -97,7 +113,7 @@ public void Main(string argument, UpdateType updateSource)
 		case 0: RefreshBlockCache(); break;
 		case 1: InteriorLightAdjust(); break;
 		case 2: ReflectorLightAdjust(); break;
-		case 3: SolarAdjust(); break;
+		case 3: SolarAdjust(); DisplayClocks(); break;
 		case 4: CleanProductionInventories(); break;
 		case 5: DisplayStorageContents(); break;
 	}
@@ -110,7 +126,6 @@ public void Main(string argument, UpdateType updateSource)
 }
 
 // ------------------------------------------------------------------------------- Cache
-
 void EchoStats()
 {
 	_sb.Clear();
@@ -127,50 +142,51 @@ void EchoStats()
 	Echo(text);
 	Me.GetSurface(0).WriteText(text);
 }
-
 // ------------------------------------------------------------------------------- Cache
-
 void RefreshBlockCache()
 {
+	GridTerminalSystem.GetBlocksOfType<IMyRefinery>(refinerys);
+	GridTerminalSystem.GetBlocksOfType<IMyAssembler>(assemblers);
+	GridTerminalSystem.GetBlocksOfType<IMySolarPanel>(solarPanels);
+	GridTerminalSystem.GetBlocksOfType<IMyMedicalRoom>(RefillPoints);
+	
+	//GridTerminalSystem.GetBlocksOfType<IMySolarFoodGenerator>(algaeFarms);
+	GridTerminalSystem.SearchBlocksOfName(AlgaeFarmKeyword, algaeFarms);
+
 	GridTerminalSystem.GetBlocksOfType<IMyInteriorLight>(InteriorLightBlocks);
+	GridTerminalSystem.GetBlocksOfType<IMyReflectorLight>(ReflectorLightBlocks);
+
 	interiorLightsCast.Clear();
 	for (int i = 0; i < InteriorLightBlocks.Count; i++) interiorLightsCast.Add(InteriorLightBlocks[i]);
-
-	GridTerminalSystem.GetBlocksOfType<IMyReflectorLight>(ReflectorLightBlocks);
 	reflectorLightsCast.Clear();
 	for (int i = 0; i < ReflectorLightBlocks.Count; i++) reflectorLightsCast.Add(ReflectorLightBlocks[i]);
 
-	GridTerminalSystem.GetBlocksOfType<IMyAssembler>(assemblers);
-	GridTerminalSystem.GetBlocksOfType<IMyRefinery>(refinerys);
-	GridTerminalSystem.SearchBlocksOfName(AlgaeFarmKeyword, algaeFarms);
-	invMatBlock  = BlockNamed(StorageMaterialsKeyword);
-	invCompBlock = BlockNamed(StorageComponentsKeyword);
-	solarPanel      = BlockNamed(SolarPanelKeyword) as IMySolarPanel;
-	solarRotor      = BlockNamed(SolarRotorKeyword) as IMyMotorStator;
-	solarController = BlockNamed(SolarPointerRCKeyword) as IMyRemoteControl;
-	var solarDisplayBlock = BlockNamed(SolarDisplayKeyword) as IMyTextSurfaceProvider;
-	solarDisplay = (solarDisplayBlock != null) ? solarDisplayBlock.GetSurface(SolarDisplaypanel) : null;
-	var matDisplayBlock = BlockNamed(StorageMaterialsDisplayKeyword) as IMyTextSurfaceProvider;
-	materialsDisplay = (matDisplayBlock != null) ? matDisplayBlock.GetSurface(StorageMaterialsDisplayPanel) : null;
-	var compDisplayBlock = BlockNamed(StorageComponentsDisplayKeyword) as IMyTextSurfaceProvider;
-	componentsDisplay = (compDisplayBlock != null) ? compDisplayBlock.GetSurface(StorageComponentsDisplayPanel) : null;
+	invMatBlock        = BlockNamed(StorageMaterialsKeyword);
+	invCompBlock       = BlockNamed(StorageComponentsKeyword);
+	solarAzimuthRotor  = BlockNamed(solarAzimuthRotorKeyword) as IMyMotorStator;
+	solarController    = BlockNamed(SolarPointerRCKeyword) as IMyRemoteControl;
+	solarCTC           = BlockNamed(SolarCTCKeyword) as IMyTurretControlBlock;
+	solarCamera        = BlockNamed(SolarCameraKeyword) as IMyCameraBlock;
+
+	solarDisplay       = GetTextSurface(BlockNamed(SolarDisplayKeyword) as IMyTextSurfaceProvider, SolarDisplaypanel);
+	materialsDisplay   = GetTextSurface(BlockNamed(StorageMaterialsDisplayKeyword) as IMyTextSurfaceProvider, StorageMaterialsDisplayPanel);
+	componentsDisplay  = GetTextSurface(BlockNamed(StorageComponentsDisplayKeyword) as IMyTextSurfaceProvider, StorageComponentsDisplayPanel);
 }
-
+IMyTextSurface GetTextSurface(IMyTextSurfaceProvider DisplayBlock, int panel = 0)
+{
+	return (DisplayBlock != null) ? DisplayBlock.GetSurface(panel) : null;
+}
 // ------------------------------------------------------------------------------- Light
-
 void InteriorLightAdjust()
 {
-	LightAdjust(interiorLightsCast, cR, cG, cB, LightRadius);
+	LightAdjust(interiorLightsCast, interiorLightColor, LightRadius);
 }
-
 void ReflectorLightAdjust()
 {
-	LightAdjust(reflectorLightsCast, 255, 255, 255, 160);
+	LightAdjust(reflectorLightsCast, reflectorLightColor, 160);
 }
-
-void LightAdjust(List<IMyLightingBlock> LightBlocks, int R = 255, int G = 255, int B = 255, int LightRadius = 10, int intensity = 5)
+void LightAdjust(List<IMyLightingBlock> LightBlocks, Color color, int LightRadius = 10, int intensity = 5)
 {
-	var color = new Color(R, G, B, 255);
 	for (int i = 0; i < LightBlocks.Count; i++)
 	{
 		LightBlocks[i].Radius = LightRadius;
@@ -178,25 +194,40 @@ void LightAdjust(List<IMyLightingBlock> LightBlocks, int R = 255, int G = 255, i
 		LightBlocks[i].Color = color;
 	}
 }
-
+// ------------------------------------------------------------------------------- Clocks
+void DisplayClocks()
+{
+	for (int i = 0; i < RefillPoints.Count; i++)
+	{
+		DisplayClock(RefillPoints[i] as IMyTextSurfaceProvider, 0);
+	}
+}
+void DisplayClock(IMyTextSurfaceProvider DisplayBlock, int panel = 0)
+{
+	var surface = GetTextSurface(DisplayBlock, panel);
+	if (surface != null)
+	{
+		surface.Script = "TSS_ClockDigital";
+		surface.ScriptBackgroundColor = backgroundColor;
+		surface.ScriptForegroundColor = foregroundColor;
+	}
+}
 // ------------------------------------------------------------------------------- Storage
-
 void DisplayStorageContents()
 {
   DisplayStoredMaterials();
 	DisplayStoredComponents();
 }
-
 void DisplayStoredComponents()
 {
+	if (invCompBlock == null) return;
 	DisplayStored(invCompBlock.GetInventory(0), componentsDisplay, "== Components ==");
 }
-
 void DisplayStoredMaterials()
 {
-	DisplayStored(invMatBlock.GetInventory(0), materialsDisplay, "== Materials ==", "{0,-10}{2:3}: {1,15:N2}");
+	if (invMatBlock == null) return;
+	DisplayStored(invMatBlock.GetInventory(0), materialsDisplay, "== Materials ==", "{0,-15}: {1,13:N2}");
 }
-
 void DisplayStored(IMyInventory inventory, IMyTextSurface display, string title, string format = "{0,-20}: {1,6:N0}")
 {
 	if (inventory != null && display != null)
@@ -205,13 +236,13 @@ void DisplayStored(IMyInventory inventory, IMyTextSurface display, string title,
 		_sb.Clear();
 		_sb.AppendLine(title);
 		inventory.GetItems(items);
+		items.Sort((a, b) => ((double)b.Amount).CompareTo((double)a.Amount));
 		for (int i = 0; i < items.Count; i++)
-			_sb.AppendLine(string.Format(format, items[i].Type.SubtypeId, (double)items[i].Amount, items[i].Type.TypeId.Contains("Ore") ? "Ore" : "   "));
+			_sb.AppendLine(string.Format(format, items[i].Type.TypeId.Contains("Ore") ? items[i].Type.SubtypeId + " Ore" : items[i].Type.SubtypeId, (double)items[i].Amount));
 		display.WriteText(_sb);
 	}
 }
 // ------------------------------------------------------------------------------- Production
-
 void CleanProductionInventories()
 {
 	IMyInventory MaterialsInventory = (invMatBlock != null) ? invMatBlock.GetInventory(0) : null;
@@ -221,7 +252,6 @@ void CleanProductionInventories()
 	CleanRefinerys(MaterialsInventory);
 	CollectAlgaeFarm(MaterialsInventory);
 }
-
 void CleanAssemblers(IMyInventory ComponentsInventory, IMyInventory MaterialsInventory)
 {
 	for (int i = 0; i < assemblers.Count; i++)
@@ -229,7 +259,6 @@ void CleanAssemblers(IMyInventory ComponentsInventory, IMyInventory MaterialsInv
 		CleanAssembler(assemblers[i] as IMyAssembler, ComponentsInventory, MaterialsInventory);
 	}
 }
-
 void CleanAssembler(IMyAssembler assembler, IMyInventory ComponentsInventory, IMyInventory MaterialsInventory)
 {
 	if (assembler != null)
@@ -264,7 +293,6 @@ void CleanAssembler(IMyAssembler assembler, IMyInventory ComponentsInventory, IM
 		}
 	}
 }
-
 void CleanRefinerys(IMyInventory MaterialsInventory)
 {
 	if (refinerys != null && MaterialsInventory != null)
@@ -275,7 +303,6 @@ void CleanRefinerys(IMyInventory MaterialsInventory)
 		}
 	}
 }
-
 void CleanRefinery(IMyRefinery refinery, IMyInventory MaterialsInventory)
 {
 	if (refinery != null)
@@ -284,9 +311,7 @@ void CleanRefinery(IMyRefinery refinery, IMyInventory MaterialsInventory)
 		MoveOneItem(refinery.GetInventory(1), MaterialsInventory);
 	}
 }
-
 // ------------------------------------------------------------------------------- Algae
-
 void CollectAlgaeFarm(IMyInventory targetInventory)
 {
 	foreach (var block in algaeFarms)
@@ -298,82 +323,94 @@ void CollectAlgaeFarm(IMyInventory targetInventory)
 		}
 	}
 }
-
 // ------------------------------------------------------------------------------- Solar
-
 void SolarAdjust()
 {
-	IMySolarPanel solar = solarPanel;
-	IMyMotorStator rotor = solarRotor;
+	if (solarPanels == null || solarPanels.Count == 0 || solarAzimuthRotor == null) return;
+	IMySolarPanel solar = solarPanels[0];
 
-	SolarConfigureAxisAligner();
+
 
 	if (solar != null)
 	{
 		SolarPowerOutputPrev = SolarPowerOutput;
 		SolarPowerOutput = solar.MaxOutput / 0.16;
 		double delta = SolarPowerOutput - SolarPowerOutputPrev;
+    double maxOutput = solar.MaxOutput * solarPanels.Count;
+		double totalOutput = solar.CurrentOutput * solarPanels.Count;
 
 		_sb.Clear();
 		_sb.AppendLine("Solar Panels");
 		_sb.AppendLine(string.Format("Power:     {0,11:P8}", SolarPowerOutput));
 		_sb.AppendLine(string.Format("Delta:     {1}{0,11:P8}", delta, (delta >= 0) ? "+" : ""));
-		_sb.AppendLine(string.Format("Generated: {0,7:F3} kW", solar.MaxOutput * 1000));
-		_sb.AppendLine(string.Format("Used:      {0,7:F3} kW", solar.CurrentOutput * 1000));
+		_sb.AppendLine(string.Format("Generated: {0,7:N3} kW", maxOutput * 1000));
+		_sb.AppendLine(string.Format("Used:      {0,7:N3} kW", totalOutput * 1000));
 
-		if (delta < 0 && Math.Abs(delta) > 3.0 * Proximity)
-		{
-			SolarRedirects++;
-			SolarDirectionSwitch = SolarDirectionSwitch * (-1.0 / SolarRedirects);
-		}
-		else
-		{
-			SolarDirectionSwitch = Math.Sign(SolarDirectionSwitch);
-			SolarRedirects = 1;
-		}
-		_sb.AppendLine(string.Format("Redirect:  {0}", SolarRedirects));
-		_sb.AppendLine(string.Format("Switch:    {0}", SolarDirectionSwitch));
-		if (rotor != null)
-		{
-			rotor.BrakingTorque = rotor.Torque;
-
-			if ((1.0 - Proximity) > SolarPowerOutput && SolarPowerOutput != 0)
+		if (solarCTC == null)
+		{		
+			SolarConfigureAxisAligner();
+			if (delta < 0 && Math.Abs(delta) > 3.0 * Proximity)
 			{
-				rotor.TargetVelocityRPM = (float)(2 * SolarDirectionSwitch * (1 - SolarPowerOutput));
+				SolarRedirects++;
+				SolarDirectionSwitch = SolarDirectionSwitch * (-1.0 / SolarRedirects);
 			}
 			else
 			{
-				rotor.TargetVelocityRPM = 0;
 				SolarDirectionSwitch = Math.Sign(SolarDirectionSwitch);
+				SolarRedirects = 1;
 			}
+			_sb.AppendLine(string.Format("Redirect:  {0}", SolarRedirects));
+			_sb.AppendLine(string.Format("Switch:    {0}", SolarDirectionSwitch));
+			if (solarAzimuthRotor != null)
+			{
+				solarAzimuthRotor.BrakingTorque = solarAzimuthRotor.Torque;
 
-			_sb.AppendLine(string.Format("Angle:     {0:F8} rad", rotor.Angle));
-			_sb.AppendLine(string.Format("Velocity:  {0:F8} RPM", rotor.TargetVelocityRPM));
+				if ((1.0 - Proximity) > SolarPowerOutput && SolarPowerOutput != 0)
+				{
+					solarAzimuthRotor.TargetVelocityRPM = (float)(2 * SolarDirectionSwitch * (1 - SolarPowerOutput));
+				}
+				else
+				{
+					solarAzimuthRotor.TargetVelocityRPM = 0;
+					SolarDirectionSwitch = Math.Sign(SolarDirectionSwitch);
+				}
+
+				_sb.AppendLine(string.Format("Angle:     {0:F8} rad", solarAzimuthRotor.Angle));
+				_sb.AppendLine(string.Format("Velocity:  {0:F8} RPM", solarAzimuthRotor.TargetVelocityRPM));
+			}
 		}
-
+		else
+		{
+			SolarConfigureCTC();
+		}
 		if (solarDisplay != null)
 			solarDisplay.WriteText(_sb);
 		else
 			Echo(_sb.ToString());
 	}
 }
-
+void SolarConfigureCTC()
+{
+	_sb.AppendLine("Solar CTC found");
+	solarCTC.Camera = solarCamera;
+	_sb.AppendLine("Solar Tracker camera set");
+	solarCTC.AzimuthRotor = solarAzimuthRotor;
+	_sb.AppendLine("Solar Azimuth rotor set");
+	solarCTC.IsSunTrackerEnabled = true;
+	_sb.AppendLine("Sun tracking enabled");
+}
 void SolarConfigureAxisAligner()
 {
-	IMyRemoteControl controller = solarController;
-
-	if (controller != null)
+	if (solarController != null)
 	{
-		Vector3D pos = controller.GetPosition();
-		controller.ClearWaypoints();
-		controller.ControlThrusters = false;
-		controller.FlightMode = FlightMode.OneWay;
-		controller.AddWaypoint(new Vector3D(pos.X, pos.Y + 1000000000, pos.Z), "SOL");
+		Vector3D pos = solarController.GetPosition();
+		solarController.ClearWaypoints();
+		solarController.ControlThrusters = false;
+		solarController.FlightMode = FlightMode.OneWay;
+		solarController.AddWaypoint(new Vector3D(pos.X, pos.Y + 1000000000, pos.Z), "SOL");
 	}
 }
-
 // ------------------------------------------------------------------------------- util
-
 void MoveAllItems(IMyInventory source, IMyInventory dest)
 {
 	if (source == null || dest == null) return;
@@ -385,7 +422,6 @@ void MoveAllItems(IMyInventory source, IMyInventory dest)
 		source.TransferItemTo(dest, i, null, true, null);
 	}
 }
-
 void MoveOneItem(IMyInventory source, IMyInventory dest)
 {
 	if (source == null || dest == null) return;
@@ -394,12 +430,10 @@ void MoveOneItem(IMyInventory source, IMyInventory dest)
 	if (items.Count > 0)
 		source.TransferItemTo(dest, 0, null, true, null);
 }
-
 double[] GetXYZ(IMyTerminalBlock block)
 {
 	return new double[] { block.GetPosition().GetDim(0), block.GetPosition().GetDim(1), block.GetPosition().GetDim(2) };
 }
-
 void SetName(IMyTerminalBlock block, string name)
 {
 	if (block != null)
@@ -407,19 +441,18 @@ void SetName(IMyTerminalBlock block, string name)
 		block.CustomName = name;
 	}
 }
-
 List<IMyTerminalBlock> BlocksNamed(String str)
 {
+	L.Clear();
 	GridTerminalSystem.SearchBlocksOfName(str, L);
 	return L;
 }
-
 IMyTerminalBlock BlockNamed(String str)
 {
-	List<IMyTerminalBlock> L = BlocksNamed(str);
+	L.Clear();
+	L = BlocksNamed(str);
 	return L.Count > 0 ? L[0] : null;
 }
-
 void assert(bool cond, String errormsg)
 {
 	if (!cond) throw new Exception(errormsg);
